a13cbf5523d7ba48383c2bd25609e74d24a2edd1
Giovanna Porto Toni <giovannaportotoni@gmail.com>
2021-10-10 16:52:00 -0300

    Create action sheet with list of websites to visit

--- Project_4/Project_4/ViewController.swift
+++ Project_4/Project_4/ViewController.swift
@@ -21,11 +21,31 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let url = URL(string: "https://www.reddit.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
 
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page:", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "reddit.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        //important for iPad. It tells iOS where the action sheet should be anchored
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)!
+        //double force unwrap because I know that the action will have a title and the resulting URL will be valid
+        webView.load(URLRequest(url: url))
+    }
 
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
